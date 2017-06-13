5afecabe32090ef6c2785d05684f0f38df96a0f2
vanncho <van_cho@abv.bg>
2017-06-13 16:54:39 +0300

    Finished Task4-1 in branch Notifications

--- scripts/prodavachnik.js
+++ scripts/prodavachnik.js
@@ -1,4 +1,9 @@
 function startApp() {
+
+    if (sessionStorage.getItem('authToken') !== null) {
+        let username = sessionStorage.getItem('username');
+        $('#loggedInUser').text("Welcome, " + username + "!");
+    }
     showHideMenuLinks();
     showHomeView();
 
@@ -31,6 +36,7 @@ function startApp() {
             $("#linkRegister").show();
             $("#linkListAds").hide();
             $("#linkLogout").hide();
+            $("#loggedInUser").hide();
         } else {
             // We have logged in user
             $("#linkLogin").hide();
@@ -85,6 +91,9 @@ function startApp() {
         sessionStorage.setItem('authToken', userAuth);
         let userId = userInfo._id;
         sessionStorage.setItem('userId', userId);
+        let username = userInfo.username;
+        sessionStorage.setItem('username', username);
+        $('#loggedInUser').text("Welcome, " + username + "!");
     }
 
     // user/register
